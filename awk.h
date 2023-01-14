130a9b3f72b130d8db4f0bfe457b45fe59e9abb5
James Halim <tirtajames45@gmail.com>
2023-01-15 00:02:01 +0700

    newest

--- awk.h
+++ awk.h
@@ -5,14 +5,13 @@ static void awkc(char delim, char inStr[], int nStr, char outStr[])
 	int i = 0;
 	while (inStr[i] == delim && inStr[i] != '\0') ++i;
 
-	if (nStr > 1) {
+	if (nStr > 1)
 		for (int nDelim = 1; nDelim<nStr && inStr[i] != '\0'; ++nDelim) {
 			while (inStr[i] != delim && inStr[i] != '\0') ++i;
 			while (inStr[i] == delim && inStr[i] != '\0') ++i;
 		}
-	}
-	for (int j = 0; inStr[i] != delim && inStr[i] !='\0' ; ++i, ++j) {
+
+	for (int j = 0; inStr[i] != delim && inStr[i] !='\0' ; ++i, ++j)
 		outStr[j] = inStr[i];
-	}
 }
 #endif
