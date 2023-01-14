b1f777707010db437366614b2d67cec1343250c1
James Halim <tirtajames45@gmail.com>
2023-01-14 11:56:11 +0700

    newest

---
+++ awk.h
@@ -0,0 +1,18 @@
+#ifndef AWK_H
+#define AWK_H
+static void cawk(char delim, char inStr[], int nStr, char outStr[])
+{
+	int i = 0;
+	while (inStr[i] == delim && inStr[i] != '\0') ++i;
+
+	if (nStr > 1) {
+		for (int nDelim = 1; nDelim<nStr && inStr[i] != '\0'; ++nDelim) {
+			while (inStr[i] != delim && inStr[i] != '\0') ++i;
+			while (inStr[i] == delim && inStr[i] != '\0') ++i;
+		}
+	}
+	for (int j = 0; inStr[i] != delim && inStr[i] !='\0' ; ++i, ++j) {
+		outStr[j] = inStr[i];
+	}
+}
+#endif
