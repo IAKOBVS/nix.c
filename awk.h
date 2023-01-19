37279b602c0b5c44fcfe579d1613fa59b8c72b75
James Halim <tirtajames45@gmail.com>
2023-01-19 15:58:43 +0700

    newest

--- awk.h
+++ awk.h
@@ -1,17 +1,18 @@
 #ifndef AWK_H
 #define AWK_H
-static void awkc(char delim, char inStr[], int nStr, char outStr[])
+static void awAwk(char delim, char inStr[], int nStr, char outStr[])
 {
 	int i = 0;
-	while (inStr[i] == delim && inStr[i] != '\0') ++i;
-
+	while (inStr[i] == delim && inStr[i])
+		++i;
 	if (nStr > 1)
-		for (int nDelim = 1; nDelim<nStr && inStr[i] != '\0'; ++nDelim) {
-			while (inStr[i] != delim && inStr[i] != '\0') ++i;
-			while (inStr[i] == delim && inStr[i] != '\0') ++i;
+		for (int nDelim = 1; nDelim<nStr && inStr[i]; ++nDelim) {
+			while (inStr[i] != delim && inStr[i])
+				++i;
+			while (inStr[i] == delim && inStr[i])
+				++i;
 		}
-
-	for (int j = 0; inStr[i] != delim && inStr[i] !='\0' ; ++i, ++j)
+	for (int j = 0; inStr[i] != delim && inStr[i]; ++i, ++j)
 		outStr[j] = inStr[i];
 }
 #endif
