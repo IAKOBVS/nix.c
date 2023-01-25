0dd02d34e626748063b225aa78573e5ec35bd4b7
James Halim <tirtajames45@gmail.com>
2023-01-25 14:04:13 +0700

    newest

--- awk.h
+++ awk.h
@@ -6,7 +6,7 @@ static void awAwk(char delim, char *inStr, int nStr, char *outStr)
 	while (inStr[i] == delim && inStr[i])
 		++i;
 	if (nStr > 1)
-		for (int nDelim = 1; nDelim<nStr && inStr[i]; ++nDelim) {
+		for (int nDelim = 1; nDelim<nStr; ++nDelim) {
 			while (inStr[i] != delim && inStr[i])
 				++i;
 			while (inStr[i] == delim && inStr[i])
