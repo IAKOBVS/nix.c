ac2b7a4206895efc9d0226b041d5d4e2a434a308
James Halim <tirtajames45@gmail.com>
2023-01-26 22:42:09 +0700

    newest

--- nix.h
+++ nix.h
@@ -1,8 +1,9 @@
-#ifndef CAT_H_DEF
-#define CAT_H_DEF
+#ifndef NIX_H_DEF
+#define NIX_H_DEF
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+
 static int sizeOfFile(char *filename)
 {
 	struct stat fileInfo;
@@ -39,48 +40,78 @@ static void cat(char *filename, char **outFile)
 	fclose(file);
 }
 
-static void awk(char delim, char *inStr, int nStr, char *outStr)
-{
-	int i = 0;
-	while (inStr[i] == delim && inStr[i]) ++i;
-	if (nStr > 1)
-		for (int nDelim = 1; nDelim<nStr; ++nDelim) {
-			while (inStr[i] != delim && inStr[i])
-				++i;
-			while (inStr[i] == delim && inStr[i])
-				++i;
-		}
-	for (int j = 0; inStr[i] != delim && inStr[i]; ++i, ++j)
-		outStr[j] = inStr[i];
-}
-
-static void awkMult(char delim, int nStr, char *filename, char **outStr)
+static void awk(char delim, int nStr, char *filename, char **outStr)
 {
 	int fileSize = sizeOfFile(filename);
-	char *fileBuff;
+	char *fileStr;
 	*outStr = malloc(fileSize);
-	cat(filename, &fileBuff);
-	if (!fileBuff)
+	cat(filename, &fileStr);
+	if (!fileStr)
 		return;
 	for (int lineNum = wcl(filename), i=0, j=0, curr=0; curr<lineNum; ++curr) {
-		while (fileBuff[i] == delim && fileBuff[i] != '\n' && i<fileSize)
-			++i;
 		if (nStr > 1)
-			for (int nDelim = 1; nDelim<nStr; ++nDelim) {
-				while (fileBuff[i] != delim && fileBuff[i] != '\n' && i<fileSize)
+			for (int n=1; n<nStr; ++n) {
+				while (fileStr[i] != delim && i<fileSize)
 					++i;
-				while (fileBuff[i] == delim && fileBuff[i] != '\n' && i<fileSize)
+				while (fileStr[i] == delim && i<fileSize)
 					++i;
 			}
-		while (fileBuff[i] != '\n') {
-			if (i >= fileSize)
+		/* assigns matched column to string */
+		while (fileStr[i] != delim && i<fileSize)
+			*outStr[j++] = fileStr[i++];
+		while (fileStr[i] != '\n')
+			if (i++<fileSize)
 				return;
-			else if (fileBuff[i] != delim)
-				(*outStr)[j++] = fileBuff[i];
-			++i;
-		}
-		(*outStr)[j++] = '\n';
+		*outStr[j] = '\n';
 	}
-	free(fileBuff);
+	free(fileStr);
 }
+
+/* static void awk(char delim, char *inStr, int nStr, char *outStr) */
+/* { */
+/* 	int i = 0; */
+/* 	while (inStr[i] == delim && inStr[i]) ++i; */
+/* 	if (nStr > 1) */
+/* 		for (int nDelim = 1; nDelim<nStr; ++nDelim) { */
+/* 			while (inStr[i] != delim && inStr[i]) */
+/* 				++i; */
+/* 			while (inStr[i] == delim && inStr[i]) */
+/* 				++i; */
+/* 		} */
+/* 	for (int j = 0; inStr[i] != delim && inStr[i]; ++i, ++j) */
+/* 		outStr[j] = inStr[i]; */
+/* } */
+
+/* static void awkMult(char delim, int nStr, char *filename, char **outStr) */
+/* { */
+/* 	int fileSize = sizeOfFile(filename); */
+/* 	char *fileBuff; */
+/* 	*outStr = malloc(fileSize); */
+/* 	cat(filename, &fileBuff); */
+/* 	if (!fileBuff) */
+/* 		return; */
+/* 	for (int lineNum = wcl(filename), i=0, j=0, curr=0; curr<lineNum; ++curr) { */
+/* 		while (fileBuff[i] == delim && fileBuff[i] != '\n' && i<fileSize) */
+/* 			++i; */
+/* 		if (nStr > 1) */
+/* 			for (int nDelim = 1; nDelim<nStr; ++nDelim) { */
+/* 				while (fileBuff[i] != delim && fileBuff[i] != '\n' && i<fileSize) */
+/* 					++i; */
+/* 				while (fileBuff[i] == delim && fileBuff[i] != '\n' && i<fileSize) */
+/* 					++i; */
+/* 			} */
+/* 		while (fileBuff[i] != '\n') { */
+/* 			if (i >= fileSize) */
+/* 				return; */
+/* 			else if (fileBuff[i] != delim) */
+/* 				(*outStr)[j++] = fileBuff[i]; */
+/* 			else */
+/* 				break; */
+/* 			++i; */
+/* 		} */
+/* 		(*outStr)[j++] = '\n'; */
+/* 	} */
+/* 	free(fileBuff); */
+/* } */
+
 #endif
