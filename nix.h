143eafa9c8a1ac9819fa67194e0c1da58f44642c
James Halim <tirtajames45@gmail.com>
2023-01-28 16:30:19 +0700

    newest

--- nix.h
+++ nix.h
@@ -1,10 +1,8 @@
 #ifndef NIX_H_DEF
 #define NIX_H_DEF
-
-static int sizeOfFile(char *filename);
-static int tee(char *flag, char *inStr, char *filename);
-static int cat(char *filename, char **outStr);
-static int wc(char flag, char *filename);
-static int awk(char delim, int nStr, char *filename, char **outStr);
-
+int sizeOfFile(char *filename);
+int tee(char *flag, char *inStr, char *filename);
+int cat(char *filename, char **outStr);
+int wc(char flag, char *filename);
+int awk(char delim, int nStr, char *filename, char **outStr);
 #endif
