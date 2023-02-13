c23c302980bfd6a8c69dd5c2bc29a2537f32e1d6
James Halim <tirtajames45@gmail.com>
2023-02-13 21:49:39 +0700

    newest

--- nix.h
+++ nix.h
@@ -10,18 +10,24 @@ extern "C" {
 
 /* get size of file */
 int nixSizeOfFile(const char *filename);
+
 /* flags: */
 /* 'w' = overwrite */
 /* 'a' = append */
 int nixTee(const char *flag, char *inStr, const char *filename);
+
 /* get first line of file */
 int nixHead(const char *filename, char **dest);
+
 /* read file into string */
 int nixCat(const char *filename, char **dest);
+
 /* find files in directory */
 int nixFindDir(char *dir, char **dest);
+
 /* get number of chars in string */
 int nixWcc(char *src);
+
 /* get number of words in string */
 int nixWcWord(char *src);
 
