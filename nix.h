d3b9f9783cea870e217ffe3d29cb86eb353c6ad5
James Halim <tirtajames45@gmail.com>
2023-02-23 19:57:12 +0700

    newest

--- nix.h
+++ nix.h
@@ -56,65 +56,56 @@ int nixWcSpace(const char *RESTRICT src);
 int nixWcTab(const char *RESTRICT src);
 int nixWcNonWords(const char *RESTRICT src);
 
-/* get number of chars in string */
-int nixWcChar(const char *RESTRICT src);
-int nixWcCharPipe(const char *RESTRICT src);
-int nixWcCharComma(const char *RESTRICT src);
-int nixWcCharDot(const char *RESTRICT src);
-int nixWcCharQuote(const char *RESTRICT src);
-int nixWcCharDoubleQuote(const char *RESTRICT src);
-int nixWcCharTab(const char *RESTRICT src);
-int nixWcCharNl(const char *RESTRICT src);
+/* /1* get number of chars in string *1/ */
+/* int nixWcChar(const char *RESTRICT src); */
+/* int nixWcCharPipe(const char *RESTRICT src); */
+/* int nixWcCharComma(const char *RESTRICT src); */
+/* int nixWcCharDot(const char *RESTRICT src); */
+/* int nixWcCharQuote(const char *RESTRICT src); */
+/* int nixWcCharDoubleQuote(const char *RESTRICT src); */
+/* int nixWcCharTab(const char *RESTRICT src); */
+/* int nixWcCharNl(const char *RESTRICT src); */
 
-int nixWcCharAlpha(const char *RESTRICT src);
-int nixWcCharAlphaPipe(const char *RESTRICT src);
-int nixWcCharAlphaComma(const char *RESTRICT src);
-int nixWcCharAlphaDot(const char *RESTRICT src);
-int nixWcCharAlphaQuote(const char *RESTRICT src);
-int nixWcCharAlphaDoubleQuote(const char *RESTRICT src);
-int nixWcCharAlphaTab(const char *RESTRICT src);
+/* int nixWcCharAlpha(const char *RESTRICT src); */
+/* int nixWcCharAlphaPipe(const char *RESTRICT src); */
+/* int nixWcCharAlphaComma(const char *RESTRICT src); */
+/* int nixWcCharAlphaDot(const char *RESTRICT src); */
+/* int nixWcCharAlphaQuote(const char *RESTRICT src); */
+/* int nixWcCharAlphaDoubleQuote(const char *RESTRICT src); */
+/* int nixWcCharAlphaTab(const char *RESTRICT src); */
 
-/* get number of words in string */
-int nixWcWord(const char *RESTRICT src);
-int nixWcWordComma(const char *RESTRICT src);
-int nixWcWordDot(const char *RESTRICT src);
-int nixWcWordPipe(const char *RESTRICT src);
-int nixWcWordQuote(const char *RESTRICT src);
-int nixWcWordDoubleQuote(const char *RESTRICT src);
-int nixWcWordTab(const char *RESTRICT src);
-int nixWcWordNl(const char *RESTRICT src);
+/* /1* get number of words in string *1/ */
+/* int nixWcWord(const char *RESTRICT src); */
+/* int nixWcWordComma(const char *RESTRICT src); */
+/* int nixWcWordDot(const char *RESTRICT src); */
+/* int nixWcWordPipe(const char *RESTRICT src); */
+/* int nixWcWordQuote(const char *RESTRICT src); */
+/* int nixWcWordDoubleQuote(const char *RESTRICT src); */
+/* int nixWcWordTab(const char *RESTRICT src); */
+/* int nixWcWordNl(const char *RESTRICT src); */
 
-int nixWcWordAlpha(const char *RESTRICT src);
-int nixWcWordAlphaComma(const char *RESTRICT src);
-int nixWcWordAlphaDot(const char *RESTRICT src);
-int nixWcWordAlphaPipe(const char *RESTRICT src);
-int nixWcWordAlphaQuote(const char *RESTRICT src);
-int nixWcWordAlphaDoubleQuote(const char *RESTRICT src);
-int nixWcWordAlphaTab(const char *RESTRICT src);
+/* int nixWcWordAlpha(const char *RESTRICT src); */
+/* int nixWcWordAlphaComma(const char *RESTRICT src); */
+/* int nixWcWordAlphaDot(const char *RESTRICT src); */
+/* int nixWcWordAlphaPipe(const char *RESTRICT src); */
+/* int nixWcWordAlphaQuote(const char *RESTRICT src); */
+/* int nixWcWordAlphaDoubleQuote(const char *RESTRICT src); */
+/* int nixWcWordAlphaTab(const char *RESTRICT src); */
 
-/* get number of words in string until newline */
-int nixWcWordTilNl(const char *RESTRICT src);
-int nixWcWordTilNlComma(const char *RESTRICT src);
-int nixWcWordTilNlDot(const char *RESTRICT src);
-int nixWcWordTilNlPipe(const char *RESTRICT src);
-int nixWcWordTilNlQuote(const char *RESTRICT src);
-int nixWcWordTilNlDoubleQuote(const char *RESTRICT src);
-int nixWcWordTilNlTab(const char *RESTRICT src);
+/* /1* get number of words in string until newline *1/ */
+/* int nixWcWordTilNl(const char *RESTRICT src); */
+/* int nixWcWordTilNlComma(const char *RESTRICT src); */
+/* int nixWcWordTilNlDot(const char *RESTRICT src); */
+/* int nixWcWordTilNlPipe(const char *RESTRICT src); */
+/* int nixWcWordTilNlQuote(const char *RESTRICT src); */
+/* int nixWcWordTilNlDoubleQuote(const char *RESTRICT src); */
+/* int nixWcWordTilNlTab(const char *RESTRICT src); */
 
-int nixAwk(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-int nixAwkComma(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-int nixAwkDot(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-int nixAwkDouble(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-int nixAwkQuote(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-int nixAwkDoubleQuote(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-int nixAwkPipe(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-int nixAwkTab(int nStr, const char *RESTRICT src, const size_t srcLen, char **RESTRICT dest);
-
-/* split spaces, tabs, etc. to array of strings */
-int nixSplitWords(const char *RESTRICT str, char ***RESTRICT arr);
-/* split lines to array of strings */
-int nixSplitNl(const char *RESTRICT str, char ***RESTRICT arr);
-void nixSplitFree(char **RESTRICT arr, size_t arrLen);
+/* /1* split spaces, tabs, etc. to array of strings *1/ */
+/* int nixSplitWords(const char *RESTRICT str, char ***RESTRICT arr); */
+/* /1* split lines to array of strings *1/ */
+/* int nixSplitNl(const char *RESTRICT str, char ***RESTRICT arr); */
+/* void nixSplitFree(char **RESTRICT arr, size_t arrLen); */
 
 #ifdef __cplusplus
 }
